edccf2091cb74ae71905ebbe847e1440c6f2d7b5
Patrycja Szostak <patrycja.szostak@globallogic.com>
2022-11-20 13:44:18 +0100

    separated methods for get dir and files

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 from utils.log_utils import setup_logger
+from utils.file_utils import File
 
 
 def compare_file_content(source_path, replica_path):
@@ -23,19 +24,34 @@ def get_content_of_dir(dir_path):
 def get_files_of_dir(dir_path):
     ''' Returns list of content in directory '''
 
-    file_list = get_content_of_dir(dir_path)
+def get_files_list_of_dir(dir_path):
+    ''' Returns list of files in directory '''
 
-    for element in file_list:
+    content_list = get_content_of_dir(dir_path)
+    file_list = []
+
+    for element in content_list:
         if os.path.isfile(f'{dir_path}{element}'):
             logging.debug(f'File: {element}')
+            file_list.append(f'{dir_path}{element}')
 
-    return [f'{dir_path}{file}' for file in file_list]
+    return file_list
 
 def get_files_with_hash(dir_path):
     ''' Returns list of obj of content in directory '''
     list_of_files = get_files_of_dir(dir_path)
 
+def get_files_from_dir(dir_path):
+    ''' Returns list of File obj from directory '''
+    list_of_files_names = get_files_list_of_dir(dir_path)
+    files = []
 
+    for path in list_of_files_names:
+        file = File(path, dir_path)
+        file.get_file_hash()
+        files.append(file)
+
+    return files
 
 
 def sync():
