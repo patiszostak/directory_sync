f266ce9e979406998453924bf8ff604392215d03
Patrycja Szostak <patrycja.szostak@globallogic.com>
2022-11-20 11:09:05 +0100

    main skeleton

---
+++ main.py
@@ -0,0 +1,70 @@
+import argparse
+import hashlib
+import os
+
+
+def compare_file_content(source_path, replica_path):
+    pass
+
+def get_content_of_dir(dir_path):
+    content = os.listdir(dir_path)
+    for element in content:
+        try:
+            ins_content = get_content_of_dir(f'{dir_path}{element}')
+            ins_content = [f'{element}\\{ins_element}' for ins_element in ins_content]
+            content.extend(ins_content)
+        except:
+            pass
+
+    return content
+
+def get_files_of_dir(dir_path):
+    ''' Returns list of content in directory '''
+
+    file_list = get_content_of_dir(dir_path)
+
+    for element in file_list:
+        if os.path.isdir(f'{dir_path}{element}'):
+            file_list.remove(element)
+
+    return [f'{dir_path}{file}' for file in file_list]
+
+
+def get_file_hash(file_content) -> str:
+    ''' Returns MD5 sum for given content'''
+    hash_code = hashlib.md5(file_content).hexdigest()
+    return hash_code
+
+
+def read_file_content(source_path) -> bytes:
+    ''' Reads file content and returns it as str '''
+
+    file_content: bytes
+    with open(source_path, 'rb') as source_file:
+        file_content = source_file.read()
+
+    return file_content
+
+
+def sync():
+    pass
+
+
+def parse_arguments():
+    ''' Parse input arguments and returns it as ArgumentParser dict '''
+    arg_pars = argparse.ArgumentParser()
+
+    arg_pars.add_argument('-p', '--sync_path', type=str, help='Folder path to synchronize')
+    arg_pars.add_argument('-i', '--interval', type=str, help='Interval to synchronize [s] 1-60')
+    arg_pars.add_argument('-l', '--log_path', type=str, help='Path to store operation logs')
+
+    return arg_pars
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    sync_path = '/\\source\\'
+    files = get_files_of_dir(sync_path)
+    for file in files:
+        print(file)
+    pass
