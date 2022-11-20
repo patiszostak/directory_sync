deb6edbbdbb141b2054eb9e69a01aaede1a65d3e
Patrycja Szostak <patrycja.szostak@globallogic.com>
2022-11-20 15:35:39 +0100

    added logic for searching for missing/changed/deleted files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+from typing import List
 
 from utils.log_utils import setup_logger
 from utils.file_utils import File
@@ -9,7 +10,9 @@ from utils.file_utils import File
 def compare_file_content(source_path, replica_path):
     pass
 
+
 def get_content_of_dir(dir_path):
+    ''' Returns list of content in directory '''
     content = os.listdir(dir_path)
     for element in content:
         try:
@@ -21,8 +24,6 @@ def get_content_of_dir(dir_path):
 
     return content
 
-def get_files_of_dir(dir_path):
-    ''' Returns list of content in directory '''
 
 def get_files_list_of_dir(dir_path):
     ''' Returns list of files in directory '''
@@ -37,9 +38,6 @@ def get_files_list_of_dir(dir_path):
 
     return file_list
 
-def get_files_with_hash(dir_path):
-    ''' Returns list of obj of content in directory '''
-    list_of_files = get_files_of_dir(dir_path)
 
 def get_files_from_dir(dir_path):
     ''' Returns list of File obj from directory '''
@@ -52,6 +50,40 @@ def get_files_from_dir(dir_path):
         files.append(file)
 
     return files
+
+
+def search_changed_files_in_replica(source_files: List[File], replica_files: List[File]):
+    '''Searches for missing or changed files in replica directory'''
+    logging.debug('Searching for missing or changed files in replica path.')
+
+    for source_file in source_files:
+        is_file_missing = True
+        is_file_changed = True
+        for replica_file in replica_files:
+            if replica_file.file_name == source_file.file_name:
+                is_file_missing = False
+                if replica_file.file_hash_md5 == source_file.file_hash_md5:
+                    is_file_changed = False
+
+        if is_file_changed:
+            pass #TODO: remove file with this name from replica
+
+        if is_file_missing or is_file_changed:
+            pass #TODO: copy file with this name from replica
+
+
+def search_deleted_files_in_replica(source_files: List[File], replica_files: List[File]):
+    '''Searches for missing or changed files in source directory'''
+    logging.debug('Searching for deleted files in replica path.')
+
+    for replica_file in replica_files:
+        is_file_deleted = True
+        for source_file in source_files:
+            if replica_file.file_name == source_file.file_name:
+                is_file_deleted = False
+
+        if is_file_deleted:
+            pass #TODO: Remove file from replica directory
 
 
 def sync():
@@ -71,9 +103,12 @@ def parse_arguments():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     setup_logger()
 
-    sync_path = 'C:\\Users\\patrycja.szostak\\Desktop\\PYTHON\\dir_sync\\source\\'
-    files = get_files_of_dir(sync_path)
+    source_path = 'C:\\Users\\patrycja.szostak\\Desktop\\PYTHON\\dir_sync\\source\\'
+    replica_path = 'C:\\Users\\patrycja.szostak\\Desktop\\PYTHON\\dir_sync\\replica\\'
+    src_files = get_files_from_dir(source_path)
+    rpl_files = get_files_from_dir(replica_path)
 
+    search_changed_files_in_replica(src_files, rpl_files)
+    search_deleted_files_in_replica(src_files, rpl_files)
